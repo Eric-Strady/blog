7b2f7aabad2723fb79afd9d564da7063da940471
Eric-Strady <strady60@gmail.com>
2018-08-26 16:02:23 +0200

    modification parametre formulaire connexion

--- connect.php
+++ connect.php
@@ -12,9 +12,9 @@
 	    
 		<form action="interfaceAdmin.php" method="post">
 			<p><label for="pseudo">Pseudo:</label><br/>
-			<input type="text" id="pseudo" name="pseudo" required/></p>
+			<input type="text" id="pseudo" name="pseudo" maxlength="255" required/></p>
 			<p><label for="password">Mot de passe:</label><br/>
-		    <input type="password" name="password" required/></p>
+		    <input type="password" name="password" maxlength="255" required/></p>
 		    <input type="submit" value="Se connecter"/>
 		</form>
 

a7a1b61c77d6a6b526cd60283bd4bd4684baff77
Eric-Strady <strady60@gmail.com>
2018-08-26 17:22:31 +0200

    ajout requete SQL pour afficher les commentaires d'un post

--- comments.php
+++ comments.php
@@ -10,9 +10,10 @@
 
 	<body>
 		<p><a href="index.php">Page d'accueil</a></p>
-		
-		<?php
 
+		<h2>Billet:</h2>
+
+		<?php
 			try
 			{
 			    $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
@@ -23,16 +24,33 @@
 			}
 
 			$req = $db->prepare('SELECT title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = :postId');
-			$req->execute(array(
-				'postId' => $_GET['post']
-			));
+			$req->execute(array('postId' => $_GET['post']));
 			$data = $req->fetch();
-
 		?>
 
 		<h4><?php echo strip_tags($data['title']); ?></h4>
 		<em>le <?php echo $data['creation_date_fr'];?></em>
 		<p><?php echo nl2br(strip_tags($data['content']));?></p>
+
+		<h2>Commentaires:</h2>
+
+		<?php
+			$req->closeCursor();
+
+			$req = $db->prepare('SELECT author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE id_post = :postId ORDER BY comment_date');
+			$req->execute(array('postId' => $_GET['post']));
+
+			while ($data = $req->fetch())
+			{
+		?>
+
+		<p><strong><?php echo strip_tags($data['author']);?></strong> le <?php echo $data['comment_date_fr'];?></p>
+		<p><?php echo nl2br(strip_tags($data['comment']));?></p>
+		
+		<?php
+			}
+			$req->closeCursor();
+		?>
 
 	</body>
 </html>

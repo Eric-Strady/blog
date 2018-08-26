df6cddb219692206652e612b664b7126ac909261
Eric-Strady <strady60@gmail.com>
2018-08-26 16:44:43 +0200

    ajout requete SQL pour affichage 5 derniers posts

--- index.php
+++ index.php
@@ -10,5 +10,29 @@
 
 	<body>
 	    
+		<?php
+			try
+			{
+			    $db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+			}
+			catch(Exception $e)
+			{
+			        die('Erreur : '.$e->getMessage());
+			}
+
+			$req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY id DESC LIMIT 0, 5');
+			while ($data = $req->fetch())
+			{
+		?>
+
+			<h4><?php echo htmlspecialchars($data['title']);?></h4>
+			<em> le <?php echo $data['creation_date_fr'];?></em>
+		    <p><?php echo nl2br(htmlspecialchars($data['content']));?><br/>
+		    <em><a href="comments.php?post=<?php echo $data['id']; ?>">Commentaires</a></em></p>
+
+		<?php
+			}
+		?>
+
 	</body>
 </html>

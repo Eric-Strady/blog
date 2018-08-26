1958a18e750c06995e734a0fb0c6e9a7f013eb17
Eric-Strady <strady60@gmail.com>
2018-08-26 17:53:52 +0200

    affichage d'une erreur si le parametre GET ne contient pas ce qui est attendu

--- comments.php
+++ comments.php
@@ -11,8 +11,6 @@
 	<body>
 		<p><a href="index.php">Page d'accueil</a></p>
 
-		<h2>Billet:</h2>
-
 		<?php
 			try
 			{
@@ -26,8 +24,12 @@
 			$req = $db->prepare('SELECT title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = :postId');
 			$req->execute(array('postId' => $_GET['post']));
 			$data = $req->fetch();
+
+			if (!empty($data))
+			{
 		?>
 
+		<h2>Billet:</h2>
 		<h4><?php echo strip_tags($data['title']); ?></h4>
 		<em>le <?php echo $data['creation_date_fr'];?></em>
 		<p><?php echo nl2br(strip_tags($data['content']));?></p>
@@ -35,6 +37,16 @@
 		<h2>Commentaires:</h2>
 
 		<?php
+			}
+			else
+			{
+		?>
+
+		<h2>Erreur:</h2>
+		<p>Le billet auquel vous souhaitez accéder n'existe pas !</p>
+
+		<?php
+			}
 			$req->closeCursor();
 
 			$req = $db->prepare('SELECT author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE id_post = :postId ORDER BY comment_date');

73581e2c0804ce0d2508d4ff021420af61ae8d8e
Eric-Strady <strady60@gmail.com>
2018-08-27 12:35:41 +0200

    ajout systeme d'ajout de commentaires et affichage des commentaires sur comments.php

--- comments.php
+++ comments.php
@@ -18,7 +18,7 @@
 			}
 			catch(Exception $e)
 			{
-			        die('Erreur : '.$e->getMessage());
+			    die('Erreur : '.$e->getMessage());
 			}
 
 			$req = $db->prepare('SELECT title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = :postId');
@@ -29,12 +29,13 @@
 			{
 		?>
 
-		<h2>Billet:</h2>
-		<h4><?php echo strip_tags($data['title']); ?></h4>
-		<em>le <?php echo $data['creation_date_fr'];?></em>
-		<p><?php echo nl2br(strip_tags($data['content']));?></p>
+				<h2>Billet:</h2>
+				<h4><?php echo strip_tags($data['title']); ?></h4>
+				<em>le <?php echo $data['creation_date_fr'];?></em>
+				<p><?php echo nl2br(strip_tags($data['content']));?></p>
+				<em><a href="comments.php?post=<?php echo $_GET['post'];?>&amp;comment=add">Ajouter un commentaire</a></em>
 
-		<h2>Commentaires:</h2>
+				<h2>Commentaires:</h2>
 
 		<?php
 			}
@@ -42,26 +43,42 @@
 			{
 		?>
 
-		<h2>Erreur:</h2>
-		<p>Le billet auquel vous souhaitez accéder n'existe pas !</p>
+				<h2>Erreur:</h2>
+				<p>Le billet auquel vous souhaitez accéder n'existe pas !</p>
 
 		<?php
 			}
 			$req->closeCursor();
 
-			$req = $db->prepare('SELECT author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y à %Hh%imin%ss\') AS comment_date_fr FROM comments WHERE id_post = :postId ORDER BY comment_date');
+			if (isset($_GET['comment']) AND $_GET['comment']=='add')
+			{
+		?>
+
+				<form action="addcomment.php" method="POST">
+				<p><label for="pseudo">Pseudo: </label><input type="text" name="pseudo" id="pseudo" value="<?php if (isset($_COOKIE['pseudo'])) {echo $_COOKIE['pseudo'];} ?>" maxlength="255" required/></p>
+		        <p><label for="comment">Commentaire: </label><input type="textarea" name="comment" id="comment" placeholder="Max 255 caractères" maxlength="255" row="4" cols="40" required/></p>
+		        <input type="hidden" name="postId" value="<?php echo $_GET['post'];?>">
+		        <input type="submit" value="Envoyer le commentaire"/>
+				</form>
+				
+		<?php		
+			}
+
+			$req = $db->prepare('
+			SELECT author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y\') AS d_comment, DATE_FORMAT(comment_date, \'%Hh%imin%ss\') AS h_comment
+			FROM comments 
+			WHERE id_post = :postId 
+			ORDER BY comment_date DESC
+			');
 			$req->execute(array('postId' => $_GET['post']));
 
 			while ($data = $req->fetch())
 			{
-		?>
-
-		<p><strong><?php echo strip_tags($data['author']);?></strong> le <?php echo $data['comment_date_fr'];?></p>
-		<p><?php echo nl2br(strip_tags($data['comment']));?></p>
-		
-		<?php
+				echo '<p><em>Le ' . $data['d_comment'] . ' à ' . $data['h_comment'] . '</em> - <strong>' .strip_tags($data['author']) . '</strong>: ' . strip_tags($data['comment']) . '</p>';
 			}
+
 			$req->closeCursor();
+
 		?>
 
 	</body>

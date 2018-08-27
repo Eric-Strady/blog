3b8dda64a4ef74b6d1cfa9cc8e7434754e0cb2fa
Eric-Strady <strady60@gmail.com>
2018-08-27 10:58:09 +0200

    indentation

--- index.php
+++ index.php
@@ -56,10 +56,10 @@
 			{
 		?>
 
-			<h4><?php echo strip_tags($data['title']);?></h4>
-			<em> le <?php echo $data['creation_date_fr'];?></em>
-		    <p><?php echo nl2br(strip_tags($data['content']));?></p>
-		    <em><a href="comments.php?post=<?php echo $data['id']; ?>">Commentaires</a></em>
+				<h4><?php echo strip_tags($data['title']);?></h4>
+				<em> le <?php echo $data['creation_date_fr'];?></em>
+			    <p><?php echo nl2br(strip_tags($data['content']));?></p>
+			    <em><a href="comments.php?post=<?php echo $data['id']; ?>">Commentaires</a></em>
 
 		<?php
 			}

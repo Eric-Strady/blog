296996bdca2e5d0a59a6e98fd7e0b2df1c3e680c
Eric-Strady <strady60@gmail.com>
2018-08-28 12:41:38 +0200

    correction type champ 'password'

--- signin.php
+++ signin.php
@@ -52,7 +52,7 @@
 			<p><label for="id_connect">Pseudo ou adresse e-mail:</label><br/>
 			<input type="text" name="id_connect" id="id_connect" value="<?php echo $id?>" maxlength="255" size="30" required autofocus/></p>
 			<p><label for="pass_connect">Mot de passe:</label><br/>
-		    <input type="pass_connect" name="pass_connect" id="pass_connect" maxlength="255" size="30" required/></p>
+		    <input type="password" name="pass_connect" id="pass_connect" maxlength="255" size="30" required/></p>
 		    <p><input type="checkbox" name="auto_connect" id="auto_connect" /> <label for="auto_connect">Se rappeler de moi</label></p>
 		    <input type="submit" value="Se connecter"/>
 		</form>

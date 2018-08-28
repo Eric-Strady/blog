0c4190cf44a5a2dd78cad95f32630a008e4f13b8
Eric-Strady <strady60@gmail.com>
2018-08-28 16:06:47 +0200

    suppression du cookie

--- addcomment.php
+++ addcomment.php
@@ -19,7 +19,6 @@
 			$addComment = $db->prepare('INSERT INTO comments(id_post, author, comment, comment_date) VALUES (:id_post, :author, :comment, NOW())');
 			$addComment->execute(array('id_post' => $_POST['postId'], 'author' => $_POST['pseudo'], 'comment' => $_POST['comment']));
 
-			setcookie('pseudo', $_POST['pseudo'], time()+10*24*3600, null, null, false, true);
 			$redirection = 'Location: http://127.0.0.1/blog/comments.php?post=' . $_POST['postId'];
 			header($redirection);
 		}

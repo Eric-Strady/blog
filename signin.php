a83c0d100efca20d023d7ea6bd51dc47f25fb211
Eric-Strady <strady60@gmail.com>
2018-08-28 12:35:40 +0200

    prise en charge redirection après validation inscription avec vérification, affichage message et préremplissage champ 'pseudo' pour connexion

--- signin.php
+++ signin.php
@@ -16,10 +16,40 @@
 			<a href="signin.php">Connexion</a>
 			<a href="signout.php">Déconnexion</a>
 		</nav>
-	    
+
+		<?php
+			if (isset($_GET['pseudo']) AND $_GET['pseudo']!='')
+			{
+				try
+				{
+					$db = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+				}
+				catch (Exception $e)
+				{
+					die('Erreur: ' . $e->getMessage());
+				}
+
+				$req = $db->prepare('SELECT pseudo FROM users WHERE pseudo = :pseudo');
+				$req->execute(array('pseudo' => $_GET['pseudo']));
+				$verifyPseudo = $req->fetch();
+
+				if ($verifyPseudo)
+				{
+					$id = strip_tags($verifyPseudo['pseudo']);
+					echo 'Votre inscription a bien été prise en compte ! Pour utiliser votre compte, veuillez vous connecter ci-dessous.';
+				}
+				else
+				{
+					echo 'Cet identifiant n\'est pas valide !';
+				}
+			}
+
+
+		?>
+
 		<form action="usersmanager.php" method="post">
 			<p><label for="id_connect">Pseudo ou adresse e-mail:</label><br/>
-			<input type="text" name="id_connect" id="id_connect" maxlength="255" size="30" required autofocus/></p>
+			<input type="text" name="id_connect" id="id_connect" value="<?php echo $id?>" maxlength="255" size="30" required autofocus/></p>
 			<p><label for="pass_connect">Mot de passe:</label><br/>
 		    <input type="pass_connect" name="pass_connect" id="pass_connect" maxlength="255" size="30" required/></p>
 		    <p><input type="checkbox" name="auto_connect" id="auto_connect" /> <label for="auto_connect">Se rappeler de moi</label></p>

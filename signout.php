f2f65d04c8bcb17f01487a024d83ad0845055753
Eric-Strady <strady60@gmail.com>
2018-08-28 15:39:10 +0200

    demarrage session et destruction de la session avec ses variables

---
+++ signout.php
@@ -0,0 +1,42 @@
+<?php
+	session_start();
+
+	if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
+	{
+		$_SESSION = array();
+
+		session_destroy();
+
+		header('Location: index.php');
+	}
+	else
+	{
+?>
+
+	<!DOCTYPE html>
+	<html lang="fr">
+		<head>
+		    <meta charset="utf-8">
+		    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+		    <title>Page de déconnexion</title>
+
+		</head>
+
+		<body>
+
+			<nav>
+				<a href="index.php">Page d'accueil</a>
+				<a href="registration.php">Inscription</a>
+				<a href="signin.php">Connexion</a>
+				<a href="signout.php">Déconnexion</a>
+			</nav>
+
+			<p>Pour être déconnecté, il faudrait d'abord se <a href="signin.php">connecter</a> ;)</p>
+
+		</body>
+	</html>
+	
+<?php
+	}
+?>

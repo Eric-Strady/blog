b8defb876f190378c9919eee1d1d8a2021a09f18
Eric-Strady <strady60@gmail.com>
2018-09-01 19:46:00 +0200

    recupération du message erreur dans une variable pour affichage

---
+++ view/frontend/errorView.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8" />
+        
+        <title>Erreur</title>
+        
+    </head>
+        
+    <body>
+
+    	<nav>
+			<a href="index.php">Page d'accueil</a>
+			<a href="registration.php">Inscription</a>
+			<a href="signin.php">Connexion</a>
+			<a href="signout.php">Déconnexion</a>
+		</nav>
+
+        <?= $errorMessage ?>
+
+    </body>
+</html>

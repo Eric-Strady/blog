f68680436af50a59ed2d4b8ffede15d5b9d89043
Eric-Strady <strady60@gmail.com>
2018-09-01 16:58:24 +0200

    rassemblement des différents controlleurs

---
+++ controller.php
@@ -0,0 +1,60 @@
+<?php
+	require ('model.php');
+
+	function listPosts()
+	{
+		$max_nb_post = 5;
+		$req = countPosts();
+		$total_post = $req->fetch();
+		$req->closeCursor();
+		$nb_post = $total_post['nb_post'];
+		$nb_page = ceil($nb_post/$max_nb_post);
+
+		if (isset($_GET['page']))
+		{
+			strip_tags($_GET['page']);
+			$_GET['page'] = (int)$_GET['page'];
+			$current_page = $_GET['page'];
+			if ($current_page == 0)
+			{
+				$current_page = 1;
+			}
+			elseif ($current_page > $nb_page)
+			{
+				$current_page = $nb_page;
+			}
+		}
+		else
+		{
+			$current_page = 1;
+		}
+
+		$first_post = ($current_page-1)*$max_nb_post;
+
+		$req = getPosts($first_post, $max_nb_post);
+
+		require ('listPostsView.php');
+	}
+
+	function post()
+	{
+		$post = getPost($_GET['post']);
+
+		if (!empty($post))
+		{
+		$comments = getComments($_GET['post']);
+		require ('comments.php');
+		}
+		else
+		{
+			echo '<h2>Erreur:</h2><p>Le billet auquel vous souhaitez accéder n\'existe pas !</p>';
+			echo 'Retour à la page d\'<a href="index.php">accueil</a>';
+		}
+	}
+
+	function insertComment()
+	{
+		addComment($_POST['postId'], $_POST['pseudo'], $_POST['comment']);
+
+		require ('comments.php');
+	}

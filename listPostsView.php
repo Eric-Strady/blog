563321c1a87cc2e833a0b50ec9510255dbbbef34
Eric-Strady <strady60@gmail.com>
2018-09-01 14:32:03 +0200

    factorisation du code avec les fonction ob_start et ob_get_clean

--- listPostsView.php
+++ listPostsView.php
@@ -1,47 +1,38 @@
-<?php
-	session_start();
-?>
+<?php session_start(); ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-	    <meta charset="utf-8">
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php $title = 'Blog de Jean Forteroche'; ?>
 
-	    <title>Blog</title>
+<?php ob_start(); ?>
 
-	</head>
+	<nav>
+		<a href="index.php">Page d'accueil</a>
+		<a href="registration.php">Inscription</a>
+		<a href="signin.php">Connexion</a>
+		<a href="signout.php">Déconnexion</a>
+	</nav>
+    
+    <?php
+		while ($data = $req->fetch())
+		{
+	?>
 
-	<body>
+			<h4><?= strip_tags($data['title']) ?></h4>
+			<em> le <?= $data['creation_date_fr'] ?></em>
+		    <p><?= nl2br(strip_tags($data['content'])) ?></p>
+		    <em><a href="comments.php?post=<?= $data['id'] ?>">Commentaires</a></em>
 
-		<nav>
-			<a href="index.php">Page d'accueil</a>
-			<a href="registration.php">Inscription</a>
-			<a href="signin.php">Connexion</a>
-			<a href="signout.php">Déconnexion</a>
-		</nav>
-	    
-	    <?php
-			while ($data = $req->fetch())
-			{
-		?>
+	<?php
+		}
+		$req->closeCursor();
 
-				<h4><?= strip_tags($data['title']) ?></h4>
-				<em> le <?= $data['creation_date_fr'] ?></em>
-			    <p><?= nl2br(strip_tags($data['content'])) ?></p>
-			    <em><a href="comments.php?post=<?= $data['id'] ?>">Commentaires</a></em>
+		echo '<p>Page: ';
+		for ($i = 1; $i <= $nb_page; $i++)
+		{
+			echo '<a href="index.php?page=' . $i . '">' . $i . '</a> > ';
+		}
+		echo '</p>';
+	?>
 
-		<?php
-			}
-			$req->closeCursor();
+<?php $content = ob_get_clean(); ?>
 
-			echo '<p>Page: ';
-			for ($i = 1; $i <= $nb_page; $i++)
-			{
-				echo '<a href="index.php?page=' . $i . '">' . $i . '</a> > ';
-			}
-			echo '</p>';
-		?>
-
-	</body>
-</html>
+<?php require('template.php'); ?>

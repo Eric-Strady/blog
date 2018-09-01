afadc214685a95dbe96e78686574f6f35e4ff97b
Eric-Strady <strady60@gmail.com>
2018-09-01 15:48:04 +0200

    factorisation du code avec les fonction ob_start et ob_get_clean

--- comments.php
+++ comments.php
@@ -1,64 +1,55 @@
-<?php
-	session_start();
-?>
+<?php session_start(); ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-	    <meta charset="utf-8">
-	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<?php $title = 'Blog de Jean Forteroche'; ?>
 
-	    <title></title>
+<?php ob_start(); ?>
 
-	</head>
+	<nav>
+		<a href="index.php">Page d'accueil</a>
+		<a href="registration.php">Inscription</a>
+		<a href="signin.php">Connexion</a>
+		<a href="signout.php">Déconnexion</a>
+	</nav>
 
-	<body>
-		<nav>
-			<a href="index.php">Page d'accueil</a>
-			<a href="registration.php">Inscription</a>
-			<a href="signin.php">Connexion</a>
-			<a href="signout.php">Déconnexion</a>
-		</nav>
+	<h2>Billet:</h2>
+	<h4><?= strip_tags($post['title']) ?></h4>
+	<em>le <?= $post['creation_date_fr'] ?></em>
+	<p><?= nl2br(strip_tags($post['content'])) ?></p>
+	<em><a href="post.php?post=<?= $_GET['post'] ?>&amp;comment=add">Ajouter un commentaire</a></em>
 
-		<h2>Billet:</h2>
-		<h4><?= strip_tags($post['title']) ?></h4>
-		<em>le <?= $post['creation_date_fr'] ?></em>
-		<p><?= nl2br(strip_tags($post['content'])) ?></p>
-		<em><a href="post.php?post=<?= $_GET['post'] ?>&amp;comment=add">Ajouter un commentaire</a></em>
+	<h2>Commentaires:</h2>
 
-		<h2>Commentaires:</h2>
+	<?php
 
-		<?php
-
-			if (isset($_GET['comment']) AND $_GET['comment']=='add')
+		if (isset($_GET['comment']) AND $_GET['comment']=='add')
+		{
+			if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
 			{
-				if (isset($_SESSION['id']) AND isset($_SESSION['pseudo']))
-				{
-		?>
+	?>
 
-				<form action="addcomment.php" method="POST">
-				<p><label for="pseudo">Pseudo: </label><br/>
-				<input type="text" name="pseudo" id="pseudo" value="<?= $_SESSION['pseudo'] ?>" maxlength="255" required autofocus/></p>
-		        <p><label for="comment">Commentaire: </label><br/>
-		        <textarea name="comment" id="comment" placeholder="Max 255 caractères" maxlength="255" row="4" cols="40" required></textarea></p>
-		        <input type="hidden" name="postId" value="<?= $_GET['post'] ?>">
-		        <input type="submit" value="Envoyer le commentaire"/>
-				</form>
-				
-		<?php
-				}
-				else
-				{
-					echo 'Vous devez être connecté(e) pour ajouter un commentaire !';
-				}
+			<form action="addcomment.php" method="POST">
+			<p><label for="pseudo">Pseudo: </label><br/>
+			<input type="text" name="pseudo" id="pseudo" value="<?= $_SESSION['pseudo'] ?>" maxlength="255" required autofocus/></p>
+	        <p><label for="comment">Commentaire: </label><br/>
+	        <textarea name="comment" id="comment" placeholder="Max 255 caractères" maxlength="255" row="4" cols="40" required></textarea></p>
+	        <input type="hidden" name="postId" value="<?= $_GET['post'] ?>">
+	        <input type="submit" value="Envoyer le commentaire"/>
+			</form>
+			
+	<?php
 			}
-
-			while ($comment = $comments->fetch())
+			else
 			{
-				echo '<p><em>Le ' . $comment['d_comment'] . ' à ' . $comment['h_comment'] . '</em> - <strong>' .strip_tags($comment['author']) . '</strong>: ' . strip_tags($comment['comment']) . '</p>';
+				echo 'Vous devez être connecté(e) pour ajouter un commentaire !';
 			}
+		}
 
-		?>
+		while ($comment = $comments->fetch())
+		{
+			echo '<p><em>Le ' . $comment['d_comment'] . ' à ' . $comment['h_comment'] . '</em> - <strong>' .strip_tags($comment['author']) . '</strong>: ' . strip_tags($comment['comment']) . '</p>';
+		}
+	?>
 
-	</body>
-</html>
+<?php $content = ob_get_clean(); ?>
+
+<?php require('template.php'); ?>

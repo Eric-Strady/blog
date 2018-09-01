cdc80076046d67bac76a5ca555ba2f19037ff2ce
Eric-Strady <strady60@gmail.com>
2018-09-01 14:48:19 +0200

    ajout des fonctions getPost et getComments

--- model.php
+++ model.php
@@ -29,3 +29,25 @@
 
 		return $req;
 	}
+
+	function getPost($post)
+	{
+		$req = $db->prepare('SELECT title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = :postId');
+		$req->execute(array('postId' => $post));
+		$data = $req->fetch();
+
+		return $data;
+	}
+
+	function getComments()
+	{
+		$req = $db->prepare('
+		SELECT author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y\') AS d_comment, DATE_FORMAT(comment_date, \'%Hh%imin%ss\') AS h_comment
+		FROM comments 
+		WHERE id_post = :postId 
+		ORDER BY comment_date DESC
+		');
+		$req->execute(array('postId' => $_GET['post']));
+		
+		return $req;
+	}

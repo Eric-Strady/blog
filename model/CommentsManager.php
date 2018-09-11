992dbf14641145d0b81242c417871cef166070ca
Eric-Strady <strady60@gmail.com>
2018-09-11 12:51:15 +0200

    passage en POO avec les fonctions concernant les commentaires + héritage Manager.php

---
+++ model/CommentsManager.php
@@ -0,0 +1,35 @@
+<?php
+
+	require_once("model/Manager.php");
+
+	class CommentsManager extends Manager
+	{
+		function getComments($postId)
+		{
+			$db = dbConnect();
+
+			$comments = $db->prepare('
+			SELECT author, comment, DATE_FORMAT(comment_date, \'%d/%m/%Y\') AS d_comment, DATE_FORMAT(comment_date, \'%Hh%imin%ss\') AS h_comment
+			FROM comments 
+			WHERE id_post = :postId 
+			ORDER BY comment_date DESC
+			');
+			$comments->execute(array('postId' => $postId));
+
+			return $comments;
+		}
+
+		function addComment($postId, $pseudo, $comment)
+		{
+			$db = dbConnect();
+
+			strip_tags($pseudo);
+			strip_tags($comment);
+
+			$addComment = $db->prepare('INSERT INTO comments(id_post, author, comment, comment_date) VALUES (:id_post, :author, :comment, NOW())');
+			$addComment->execute(array('id_post' => $postId, 'author' => $pseudo, 'comment' => $comment));
+
+			$redirection = 'Location: http://127.0.0.1/blog/index.php?post=' . $postId;
+			header($redirection);
+		}
+	}

5358ab5c336fca474c0c3ed64795835eb907b4f3
Eric-Strady <strady60@gmail.com>
2018-09-11 12:52:01 +0200

    passage en POO avec les fonctions concernant les posts + héritage Manager.php

---
+++ model/PostsManager.php
@@ -0,0 +1,35 @@
+<?php
+
+	require_once("model/Manager.php");
+
+	class PostsManager extends Manager
+	{
+		function countPosts()
+		{
+			$db = dbConnect();
+
+			$req = $db->query('SELECT COUNT(*) AS nb_post FROM posts');
+
+			return $req;
+		}
+
+		function getPosts($first_post, $max_nb_post)
+		{
+			$db = dbConnect();
+
+			$req = $db->query('SELECT id, title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts ORDER BY id DESC LIMIT ' . $first_post . ', ' . $max_nb_post);
+
+			return $req;
+		}
+
+		function getPost($postId)
+		{
+			$db = dbConnect();
+
+			$req = $db->prepare('SELECT title, content, DATE_FORMAT(creation_date, \'%d/%m/%Y à %Hh%imin%ss\') AS creation_date_fr FROM posts WHERE id = :postId');
+			$req->execute(array('postId' => $postId));
+			$post = $req->fetch();
+
+			return $post;
+		}
+	}

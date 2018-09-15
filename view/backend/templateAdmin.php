613c85b19bb4a6118ac54a91169ef8a2f748b189
Eric-Strady <strady60@gmail.com>
2018-09-15 14:20:58 +0200

    ajout d'un titre et d'un menu

---
+++ view/backend/templateAdmin.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title><?= $title ?></title>
+        
+    </head>
+        
+    <body>
+
+    	<h1>Interface d'administration</h1>
+
+		<nav>
+			<ul>
+				<li><a href="#">Ajouter un billet</a></li>;
+				<li><a href="#">Modérer les commentaires</a></li>;
+				<li><a href="index.php">Visualiser le site</a></li>
+				<li><a href="#">Mon profil</a></li>;
+			</ul>
+		</nav>
+
+        <?= $content ?>
+
+    </body>
+</html>

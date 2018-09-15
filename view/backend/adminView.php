064747c93968ff0ffe5405b64aeb91261d092050
Eric-Strady <strady60@gmail.com>
2018-09-15 14:50:18 +0200

    affichage des posts

---
+++ view/backend/adminView.php
@@ -0,0 +1,23 @@
+<?php $title = 'Blog de Jean Forteroche'; ?>
+
+<?php ob_start(); ?>
+
+<?php
+	while ($data = $req->fetch())
+	{
+?>
+
+		<h4><?= strip_tags($data['title']) ?></h4>
+		<em> le <?= $data['creation_date_fr'] ?></em>
+	    <p><?= nl2br(strip_tags($data['content'])) ?></p>
+	    <em><a href="index.php?post=<?= $data['id'] ?>">Commentaires</a></em>
+
+<?php
+	}
+	$req->closeCursor();
+?>
+
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('view/backend/templateAdmin.php'); ?>

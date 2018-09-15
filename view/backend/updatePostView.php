d83d753b1a0b68e169aa8e12ae02e2e6ed30d3a3
Eric-Strady <strady60@gmail.com>
2018-09-15 16:01:08 +0200

    affichage du post désiré

--- view/backend/updatePostView.php
+++ view/backend/updatePostView.php
@@ -1,1 +1,14 @@
-<?php
+<?php session_start(); ?>
+
+<?php $title = 'Blog de Jean Forteroche'; ?>
+
+<?php ob_start(); ?>
+
+<h2>Billet:</h2>
+<h4><?= strip_tags($post['title']) ?></h4>
+<em>le <?= $post['creation_date_fr'] ?></em>
+<p><?= nl2br(strip_tags($post['content'])) ?></p>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('view/backend/templateAdmin.php'); ?>

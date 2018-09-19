c1866e5f6e9cd0c1b1e1ae7f1a9b42f4404041e0
Eric-Strady <strady60@gmail.com>
2018-09-19 15:25:18 +0200

    ajout d'un lien reinitialiser un mdp

--- view/frontend/signInView.php
+++ view/frontend/signInView.php
@@ -16,7 +16,8 @@
 	<input type="text" name="id_connect" id="id_connect" <?php if(isset($_COOKIE['pseudo'])){ echo 'value=' . $_COOKIE['pseudo'];}?> maxlength="255" size="30" required autofocus/></p>
 	<p><label for="pass_connect">Mot de passe:</label><br/>
     <input type="password" name="pass_connect" id="pass_connect" maxlength="255" size="30" required/></p>
-    <p><input type="checkbox" name="auto_connect" id="auto_connect" /> <label for="auto_connect">Se rappeler de moi</label></p>
+    <p><input type="checkbox" name="auto_connect" id="auto_connect" /> <label for="auto_connect">Se rappeler de moi (identifiant)</label></p>
+    <p><a href="index.php?link=new_password">Mot de passe oublié</a></p>
     <input type="submit" value="Se connecter"/>
 </form>
 

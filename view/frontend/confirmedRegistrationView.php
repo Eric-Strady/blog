4c546ec74f7b2299c323dba170300169673009fe
Eric-Strady <strady60@gmail.com>
2018-09-19 20:10:51 +0200

    texte de bienvenue

---
+++ view/frontend/confirmedRegistrationView.php
@@ -0,0 +1,14 @@
+<?php session_start(); ?>
+
+<?php $title = 'Réinitialiser son mot de passe'; ?>
+
+<?php ob_start(); ?>
+
+<h1>Inscription finalisée</h1>
+
+<p>Félicitation !<br/>
+Votre inscription est désormais finalisée. Vous pouvez dès à présent vous <a href="index.php?link=connexion">connecter</a> ;)</p>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('view/frontend/template.php'); ?>

519c9269585caede6bf277604f91c27c9bc55f21
Eric-Strady <strady60@gmail.com>
2018-09-20 14:12:42 +0200

    ajout formulaire pour changer de pseudo

--- view/backend/adminAccountView.php
+++ view/backend/adminAccountView.php
@@ -4,11 +4,20 @@
 
 <?php ob_start(); ?>
 
-<h1>Profil Administrateur</h1>
+<h1>Profil Administrateur:</h1>
 
 <p><?= $_SESSION['pseudo'] ?></p>
 
 <img src="public/images/univers.jpg" alt="Avatar" title="Avatar <?= $_SESSION['pseudo'] ?>" height="200" width="300">
+
+<h2>Paramètres de votre compte:</h2>
+
+<form action="index.php" method="POST">
+	<p><label for="new_pseudo">Changer mon pseudo:</label></p>
+	<p><input type="text" name="new_pseudo" id="new_pseudo" maxlength="255" size="40" required/>
+	<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
+    <input type="submit" value="Soumettre"/></p>
+</form>
 
 <?php $content = ob_get_clean(); ?>
 

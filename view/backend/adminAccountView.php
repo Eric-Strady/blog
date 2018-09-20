8ef58a79287df73c75a50357fcc5b450bc942cb0
Eric-Strady <strady60@gmail.com>
2018-09-20 13:57:34 +0200

    affichage dynamique du pseudo et affichage avatar (img) de l'utilisateur

--- view/backend/adminAccountView.php
+++ view/backend/adminAccountView.php
@@ -4,7 +4,11 @@
 
 <?php ob_start(); ?>
 
+<h1>Profil Administrateur</h1>
 
+<p><?= $_SESSION['pseudo'] ?></p>
+
+<img src="public/images/univers.jpg" alt="Avatar" title="Avatar <?= $_SESSION['pseudo'] ?>" height="200" width="300">
 
 <?php $content = ob_get_clean(); ?>
 

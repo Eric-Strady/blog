715431e3c2244c7d2983815f4cedfd5825518b94
Eric-Strady <strady60@gmail.com>
2018-09-21 10:14:48 +0200

    ajout phrase de sécurité pour la modification du mot de passe + correction code des formulaires

--- view/backend/adminAccountView.php
+++ view/backend/adminAccountView.php
@@ -17,10 +17,10 @@
 		<legend>Changer de pseudo</legend>
 		<p><label for="new_pseudo">Nouveau pseudo: </label>
 		<input type="text" name="new_pseudo" id="new_pseudo" maxlength="255" size="40" required/></p>
-		<p><label for="password"> Merci de confirmer votre mot de passe: </label>
+		<p><label for="password">Merci de confirmer votre mot de passe: </label>
 	    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
 		<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
-	    <input type="submit" value="Soumettre"/></p>
+	    <p><input type="submit" value="Soumettre"/></p>
 	</fieldset>
 </form></p>
 
@@ -29,10 +29,10 @@
 		<legend>Changer d'adresse e-mail</legend>
 		<p><label for="new_email">Nouvelle adresse e-mail: </label>
 		<input type="text" name="new_email" id="new_email" maxlength="255" size="40" required/></p>
-		<p><label for="password"> Merci de confirmer votre mot de passe: </label>
+		<p><label for="password">Merci de confirmer votre mot de passe: </label>
 	    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
 		<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
-	    <input type="submit" value="Soumettre"/></p>
+	    <p><input type="submit" value="Soumettre"/></p>
 	</fieldset>
 </form></p>
 
@@ -41,14 +41,23 @@
 		<legend>Changer de mot de passe</legend>
 		<p><label for="old_password">Ancien mot de passe: </label>
 		<input type="password" name="old_password" id="old_password" maxlength="255" size="40" required/></p>
-		<p><label for="change_password"> Nouveau mot de passe: </label>
+		<p><label for="change_password">Nouveau mot de passe *: </label>
 	    <input type="password" name="change_password" id="change_password" maxlength="255" size="40" required/></p>
-	    <p><label for="confirm_change_password"> Nouveau mot de passe: </label>
+	    <p><label for="confirm_change_password">Confirmez votre nouveau mot de passe: </label>
 	    <input type="password" name="confirm_change_password" id="confirm_change_password" maxlength="255" size="40" required/></p>
 		<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
-	    <input type="submit" value="Soumettre"/></p>
+	    <p><input type="submit" value="Soumettre"/></p>
+
+	    <p>* Pour rappel, votre mot de passe doit:<br/>
+	    <ul>
+	    	<li>Utiliser au minimum 8 caractères</li>
+	    	<li>Utiliser au minimum un chiffre et une lettre</li>
+	    	<li>Utiliser au minimum une majuscule</li>
+	    	<li>Utiliser au minimum un caractère spécial</li>
+	    </ul></p>
+
 	</fieldset>
-</form></p>
+</form>
 
 <?php $content = ob_get_clean(); ?>
 

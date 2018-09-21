16b60f36bef86dcb3063f4ffb1e88d5c7b5bae4d
Eric-Strady <strady60@gmail.com>
2018-09-21 09:09:50 +0200

    amélioration de la structure du formulaire pour chager son pseudo

--- view/backend/adminAccountView.php
+++ view/backend/adminAccountView.php
@@ -13,12 +13,15 @@
 <h2>Paramètres de votre compte:</h2>
 
 <form action="index.php" method="POST">
-	<p><label for="new_pseudo">Changer mon pseudo:</label></p>
-	<p><input type="text" name="new_pseudo" id="new_pseudo" maxlength="255" size="40" required/>
-	<p><label for="password">Merci de confirmer votre mot de passe:</label><br/>
-    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
-	<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
-    <input type="submit" value="Soumettre"/></p>
+	<fieldset>
+		<legend>Changer mon pseudo</legend>
+		<p><label for="new_pseudo">Nouveau pseudo: </label>
+		<input type="text" name="new_pseudo" id="new_pseudo" maxlength="255" size="40" required/></p>
+		<p><label for="password"> Merci de confirmer votre mot de passe: </label>
+	    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
+		<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
+	    <input type="submit" value="Soumettre"/></p>
+	</fieldset>
 </form>
 
 <?php $content = ob_get_clean(); ?>

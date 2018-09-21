961fb8f797b68c8a4cb8468a63f14d3157725cc1
Eric-Strady <strady60@gmail.com>
2018-09-21 17:09:30 +0200

    amélioration affichage avec bouton dynamique pour afficher le formulaire voulu

--- view/backend/adminAccountView.php
+++ view/backend/adminAccountView.php
@@ -29,75 +29,150 @@
 	}
 ?>
 
-<p><form action="index.php" method="POST">
-	<fieldset>
-		<legend>Changer de pseudo</legend>
-		<p><label for="new_pseudo">Nouveau pseudo: </label>
-		<input type="text" name="new_pseudo" id="new_pseudo" maxlength="255" size="40" required/></p>
-		<p><label for="password">Merci de confirmer votre mot de passe: </label>
-	    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
-		<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
-	    <p><input type="submit" value="Soumettre"/></p>
-	</fieldset>
-</form></p>
+<?php
+	if (isset($_GET['form']) AND $_GET['form']=='pseudo')
+	{
+?>
+		<p><form action="index.php" method="POST">
+			<fieldset>
+				<legend>Changer de pseudo</legend>
+				<p><label for="new_pseudo">Nouveau pseudo: </label>
+				<input type="text" name="new_pseudo" id="new_pseudo" maxlength="255" size="40" required/></p>
+				<p><label for="password">Merci de confirmer votre mot de passe: </label>
+			    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
+				<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
+			    <p><input type="submit" value="Soumettre"/></p>
+			</fieldset>
+		</form></p>
+<?php
+	}
+	else
+	{
+?>
+		<p><form action="index.php" method="POST">
+	 		<button type="submit" formaction="http://127.0.0.1/blog/index.php?link=admin_account&form=pseudo">Changer de pseudo</button>
+		</form></p>
+<?php
+	}
+?>
 
-<p><form action="index.php" method="POST">
-	<fieldset>
-		<legend>Changer d'adresse e-mail</legend>
-		<p><label for="new_email">Nouvelle adresse e-mail: </label>
-		<input type="text" name="new_email" id="new_email" maxlength="255" size="40" required/></p>
-		<p><label for="password">Merci de confirmer votre mot de passe: </label>
-	    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
-		<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
-	    <p><input type="submit" value="Soumettre"/></p>
-	</fieldset>
-</form></p>
+<?php
+	if (isset($_GET['form']) AND $_GET['form']=='email')
+	{
+?>
+		<p><form action="index.php" method="POST">
+			<fieldset>
+				<legend>Changer d'adresse e-mail</legend>
+				<p><label for="new_email">Nouvelle adresse e-mail: </label>
+				<input type="text" name="new_email" id="new_email" maxlength="255" size="40" required/></p>
+				<p><label for="password">Merci de confirmer votre mot de passe: </label>
+			    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
+				<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
+			    <p><input type="submit" value="Soumettre"/></p>
+			</fieldset>
+		</form></p>
+<?php
+	}
+	else
+	{
+?>
+		<p><form action="index.php" method="POST">
+	 		<button type="submit" formaction="http://127.0.0.1/blog/index.php?link=admin_account&form=email">Changer d'adresse e-mail</button>
+		</form></p>
+<?php
+	}
+?>
 
-<p><form action="index.php" method="POST">
-	<fieldset>
-		<legend>Changer de mot de passe</legend>
-		<p><label for="old_password">Ancien mot de passe: </label>
-		<input type="password" name="old_password" id="old_password" maxlength="255" size="40" required/></p>
-		<p><label for="change_password">Nouveau mot de passe *: </label>
-	    <input type="password" name="change_password" id="change_password" maxlength="255" size="40" required/></p>
-	    <p><label for="confirm_change_password">Confirmez votre nouveau mot de passe: </label>
-	    <input type="password" name="confirm_change_password" id="confirm_change_password" maxlength="255" size="40" required/></p>
-		<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
-	    <p><input type="submit" value="Soumettre"/></p>
+<?php
+	if (isset($_GET['form']) AND $_GET['form']=='password')
+	{
+?>
+		<p><form action="index.php" method="POST">
+			<fieldset>
+				<legend>Changer de mot de passe</legend>
+				<p><label for="old_password">Ancien mot de passe: </label>
+				<input type="password" name="old_password" id="old_password" maxlength="255" size="40" required/></p>
+				<p><label for="change_password">Nouveau mot de passe *: </label>
+			    <input type="password" name="change_password" id="change_password" maxlength="255" size="40" required/></p>
+			    <p><label for="confirm_change_password">Confirmez votre nouveau mot de passe: </label>
+			    <input type="password" name="confirm_change_password" id="confirm_change_password" maxlength="255" size="40" required/></p>
+				<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
+			    <p><input type="submit" value="Soumettre"/></p>
 
-	    <p>* Pour rappel, votre mot de passe doit:<br/>
-	    <ul>
-	    	<li>Utiliser au minimum 8 caractères</li>
-	    	<li>Utiliser au minimum un chiffre et une lettre</li>
-	    	<li>Utiliser au minimum une majuscule</li>
-	    	<li>Utiliser au minimum un caractère spécial</li>
-	    </ul></p>
-	</fieldset>
-</form></p>
+			    <p>* Pour rappel, votre mot de passe doit:<br/>
+			    <ul>
+			    	<li>Utiliser au minimum 8 caractères</li>
+			    	<li>Utiliser au minimum un chiffre et une lettre</li>
+			    	<li>Utiliser au minimum une majuscule</li>
+			    	<li>Utiliser au minimum un caractère spécial</li>
+			    </ul></p>
+			</fieldset>
+		</form></p>
+<?php
+	}
+	else
+	{
+?>
+		<p><form action="index.php" method="POST">
+	 		<button type="submit" formaction="http://127.0.0.1/blog/index.php?link=admin_account&form=password">Changer de mot de passe</button>
+		</form></p>
+<?php
+	}
+?>
 
-<p><form action="index.php" method="POST">
-	<fieldset>
-		<legend>Supprimer un compte utilisateur</legend>
-		<p><label for="user_pseudo">Pseudo de l'utilisateur: </label>
-		<input type="text" name="user_pseudo" id="user_pseudo" maxlength="255" size="40" required/></p>
-		<p><label for="reasons_suppression">Motif de suppression:</label><br/>
-		<textarea name="reasons_suppression" id="reasons_suppression" placeholder="Limité à 255 caractères" maxlength="255" row="4" cols="40" required></textarea>
-		<p><label for="password">Merci de confirmer votre mot de passe: </label>
-	    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
-		<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
-	    <p><input type="submit" value="Supprimer"/></p>
-	</fieldset>
-</form></p>
+<?php
+	if (isset($_GET['form']) AND $_GET['form']=='delete_user')
+	{
+?>
+		<p><form action="index.php" method="POST">
+			<fieldset>
+				<legend>Supprimer un compte utilisateur</legend>
+				<p><label for="user_pseudo">Pseudo de l'utilisateur: </label>
+				<input type="text" name="user_pseudo" id="user_pseudo" maxlength="255" size="40" required/></p>
+				<p><label for="reasons_suppression">Motif de suppression:</label><br/>
+				<textarea name="reasons_suppression" id="reasons_suppression" placeholder="Limité à 255 caractères" maxlength="255" row="4" cols="40" required></textarea>
+				<p><label for="password">Merci de confirmer votre mot de passe: </label>
+			    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
+				<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
+			    <p><input type="submit" value="Supprimer"/></p>
+			</fieldset>
+		</form></p>
+<?php
+	}
+	else
+	{
+?>
+		<p><form action="index.php" method="POST">
+	 		<button type="submit" formaction="http://127.0.0.1/blog/index.php?link=admin_account&form=delete_user">Supprimer un compte utilisateur</button>
+		</form></p>
+<?php
+	}
+?>
 
-<p><form action="index.php" method="POST">
-	<fieldset>
-		<legend>Supprimer son compte</legend>
-		<p><label for="password">Merci de confirmer votre mot de passe: </label>
-	    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
-		<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
-	    <p><input type="submit" value="Supprimer"/></p>
-	</fieldset>
-</form></p>
+<?php
+	if (isset($_GET['form']) AND $_GET['form']=='delete')
+	{
+?>
+		<p><form action="index.php" method="POST">
+			<fieldset>
+				<legend>Supprimer son compte</legend>
+				<p><label for="password">Merci de confirmer votre mot de passe: </label>
+			    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
+				<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
+			    <p><input type="submit" value="Supprimer"/></p>
+			</fieldset>
+		</form></p>
+<?php
+	}
+	else
+	{
+?>
+		<p><form action="index.php" method="POST">
+	 		<button type="submit" formaction="http://127.0.0.1/blog/index.php?link=admin_account&form=delete">Supprimer son compte</button>
+		</form></p>
+<?php
+	}
+?>
 
 <?php $content = ob_get_clean(); ?>
 

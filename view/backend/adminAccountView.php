7160e69c0c34d2a95f7a8938bb014f8e8b649044
Eric-Strady <strady60@gmail.com>
2018-09-21 09:04:10 +0200

    ajout d'un champ 'password' pour le formulaire de changement pseudo

--- view/backend/adminAccountView.php
+++ view/backend/adminAccountView.php
@@ -15,6 +15,8 @@
 <form action="index.php" method="POST">
 	<p><label for="new_pseudo">Changer mon pseudo:</label></p>
 	<p><input type="text" name="new_pseudo" id="new_pseudo" maxlength="255" size="40" required/>
+	<p><label for="password">Merci de confirmer votre mot de passe:</label><br/>
+    <input type="password" name="password" id="password" maxlength="255" size="40" required/></p>
 	<input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>">
     <input type="submit" value="Soumettre"/></p>
 </form>

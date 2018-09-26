aa6c05ec16a0f2738a4e697f71da11231421683e
Eric-Strady <strady60@gmail.com>
2018-09-26 20:24:12 +0200

    modifications style liste posts

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -4,20 +4,32 @@
 
 <?php ob_start(); ?>
 
-<?php
-	while ($data = $req->fetch())
-	{
-?>
+<div class="row">
+	<?php
+		while ($data = $req->fetch())
+		{
+	?>
+		    <div class="col-lg-4 col-sm-6 portfolio-item">
+		      	<div class="card h-100">
+		        	<a href="#"><img class="card-img-top" src="public/images/header.jpg" alt="En-tête billet"></a>
+		        	<div class="card-body">
+		      			<h4 class="card-title">
+		        			<a href="#"><?= strip_tags($data['title']) ?></a>
+		      			</h4>
+		      			<em> le <?= $data['creation_date_fr'] ?></em>
+		      			<em><a href="index.php?read=<?= $data['id'] ?>">Lire</a></em>
+		     			- <em><a href="index.php?update=<?= $data['id'] ?>">Mettre à jour</a></em>
+		     			- <em><a href="index.php?delete=<?= $data['id'] ?>">Supprimer</a></em></p>
+		      			<p class="card-text"><?= substr($data['content'], 0, 45) . '...' ?></p>
+		        	</div>
+		      	</div>
+		    </div>
+	<?php
+		}
+	?>
+</div>
 
-		<h4><?= strip_tags($data['title']) ?></h4>
-		<em> le <?= $data['creation_date_fr'] ?></em>
-		<em><a href="index.php?read=<?= $data['id'] ?>">Lire</a></em>
-	     - <em><a href="index.php?update=<?= $data['id'] ?>">Mettre à jour</a></em>
-	     - <em><a href="index.php?delete=<?= $data['id'] ?>">Supprimer</a></em></p>
-	    <p><?= nl2br($data['content']) ?>
-
 <?php
-	}
 	$req->closeCursor();
 	echo '<p>Page: ';
 	for ($i = 1; $i <= $nb_page; $i++)
--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -5,31 +5,28 @@
 <?php ob_start(); ?>
 
 <div class="row">
-
-<?php
-	while ($data = $req->fetch())
-	{
-?>
-	    <div class="col-lg-4 col-sm-6 portfolio-item">
-	      	<div class="card h-100">
-	        	<a href="#"><img class="card-img-top" src="public/images/header.jpg" alt="En-tête billet"></a>
-	        	<div class="card-body">
-	      			<h4 class="card-title">
-	        			<a href="#"><?= strip_tags($data['title']) ?></a>
-	      			</h4>
-	      			<em> le <?= $data['creation_date_fr'] ?></em>
-	      			<p class="card-text"><?= substr($data['content'], 0, 45) . '...' ?></p>
-	      			<div class="card-footer">
-	      				<a class="btn btn-primary" href="index.php?post=<?= $data['id'] ?>">Lire la suite</a>
-	      			</div>
-	        	</div>
-	      	</div>
-	    </div>
-
-<?php
-	}
-?>
-
+	<?php
+		while ($data = $req->fetch())
+		{
+	?>
+		    <div class="col-lg-4 col-sm-6 portfolio-item">
+		      	<div class="card h-100">
+		        	<a href="#"><img class="card-img-top" src="public/images/header.jpg" alt="En-tête billet"></a>
+		        	<div class="card-body">
+		      			<h4 class="card-title">
+		        			<a href="#"><?= strip_tags($data['title']) ?></a>
+		      			</h4>
+		      			<em> le <?= $data['creation_date_fr'] ?></em>
+		      			<p class="card-text"><?= substr($data['content'], 0, 45) . '...' ?></p>
+		      			<div class="card-footer">
+		      				<a class="btn btn-primary" href="index.php?post=<?= $data['id'] ?>">Lire la suite</a>
+		      			</div>
+		        	</div>
+		      	</div>
+		    </div>
+	<?php
+		}
+	?>
 </div>
 
 <?php

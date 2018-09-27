cc90885710fe49ba893b111e5414c5f1f8eaeb8b
Eric-Strady <strady60@gmail.com>
2018-09-27 12:58:28 +0200

    ajout style pour page de connexion

--- view/frontend/signInView.php
+++ view/frontend/signInView.php
@@ -12,15 +12,18 @@
 	}
 ?>
 
-<form action="index.php" method="post">
-	<p><label for="id_connect">Pseudo ou adresse e-mail:</label><br/>
-	<input type="text" name="id_connect" id="id_connect" <?php if(isset($_COOKIE['pseudo'])){ echo 'value=' . $_COOKIE['pseudo'];}?> maxlength="255" size="30" required autofocus/></p>
-	<p><label for="pass_connect">Mot de passe:</label><br/>
-    <input type="password" name="pass_connect" id="pass_connect" maxlength="255" size="30" required/></p>
-    <p><input type="checkbox" name="auto_connect" id="auto_connect" /> <label for="auto_connect">Se rappeler de moi (identifiant)</label></p>
-    <p><a href="index.php?link=new_password">Mot de passe oublié</a></p>
-    <input type="submit" value="Se connecter"/>
-</form>
+<div class="text-center mt-5">
+	<form class="form-signin" action="index.php" method="post">
+		<h1 class="h3 mb-3 font-weight-normal">Connexion</h1>
+		<p><label for="id_connect">Pseudo ou adresse e-mail:</label><br/>
+		<input class="form-control" type="text" name="id_connect" id="id_connect" <?php if(isset($_COOKIE['pseudo'])){ echo 'value=' . $_COOKIE['pseudo'];}?> maxlength="255" required autofocus/></p>
+		<p><label for="pass_connect">Mot de passe:</label><br/>
+	    <input class="form-control" type="password" name="pass_connect" id="pass_connect" maxlength="255" required/></p>
+	    <p><input type="checkbox" name="auto_connect" id="auto_connect" /> <label for="auto_connect">Se rappeler de moi (identifiant)</label></p>
+	    <p><a href="index.php?link=new_password">Mot de passe oublié</a></p>
+	    <input class="btn btn-lg btn-primary btn-block mb-5" type="submit" value="Se connecter"/>
+	</form>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 

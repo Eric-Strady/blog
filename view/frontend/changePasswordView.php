096752d55c184d04e3446ee47a367f08220c5072
Eric-Strady <strady60@gmail.com>
2018-10-01 15:31:33 +0200

    custom text form

--- view/frontend/changePasswordView.php
+++ view/frontend/changePasswordView.php
@@ -10,12 +10,12 @@
 	<h1>Réinitialiser son mot de passe</h1>
 
 	<p>Vous pouvez désormais choisir un nouveau mot de passe pour votre compte. Merci de respecter les règles de sécurité pour votre nouveau mot de passe.</p>
-	
+
     <div class="text-center mt-5">
         <form class="form-registration" action="index.php" method="post">
-        	<p><label for="password">Votre mot de passe *:</label><br/>
+        	<p><label for="password">Nouveau mot de passe *:</label><br/>
             <input class="form-control" type="password" name="password" id="password" maxlength="255" size="30" required/></p>
-            <p><label for="passwordVerify">Confirmez votre mot de passe:</label><br/>
+            <p><label for="passwordVerify">Confirmez votre nouveau mot de passe:</label><br/>
             <input class="form-control" type="password" name="passwordVerify" id="passwordVerify" maxlength="255" size="30" required/></p>
             <input class="btn btn-lg btn-primary btn-block" type="submit" value="S'inscrire"/>
         </form>

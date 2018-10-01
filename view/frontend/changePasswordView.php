92ff7a2320615f7f4fc3b838d3a98bd3d5ddd33a
Eric-Strady <strady60@gmail.com>
2018-10-01 15:52:58 +0200

    corrected '-' by '_'

--- view/frontend/changePasswordView.php
+++ view/frontend/changePasswordView.php
@@ -18,8 +18,8 @@
 		        <form class="form-registration" action="index.php" method="post">
 		        	<p><label for="reset_password">Nouveau mot de passe *:</label><br/>
 		            <input class="form-control" type="password" name="reset_password" id="reset_password" maxlength="255" size="30" required autofocus/></p>
-		            <p><label for="confirm-reset-password">Confirmez votre nouveau mot de passe:</label><br/>
-		            <input class="form-control" type="password" name="confirm-reset-password" id="confirm-reset-password" maxlength="255" size="30" required/></p>
+		            <p><label for="confirm_reset_password">Confirmez votre nouveau mot de passe:</label><br/>
+		            <input class="form-control" type="password" name="confirm_reset_password" id="confirm_reset_password" maxlength="255" size="30" required/></p>
 		            <input type="hidden" name="email" value="<?= $_COOKIE['email'] ?>"/>
 		            <input class="btn btn-lg btn-primary btn-block" type="submit" value="S'inscrire"/>
 		        </form>

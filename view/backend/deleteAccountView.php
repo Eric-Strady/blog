fd2772c60b3385a0700f4fb60767547af9717715
Eric-Strady <strady60@gmail.com>
2018-10-05 01:03:08 +0200

    add style for deleteAccountView.php

--- view/backend/deleteAccountView.php
+++ view/backend/deleteAccountView.php
@@ -7,23 +7,33 @@
 <?php
 	if (isset($_SESSION['admin']) AND $_SESSION['admin']=='ok')
 	{
-		echo '<h2>Suppression du compte administrateur:</h2>';
+?>
+		<div class="container">
+			<div id="transparency">
+				<h3 id="post-title" class="mb-4">Suppression du compte administrateur:</h3>
+<?php
 	}
 	else
 	{
-		echo '<h2>Suppression du compte utilisateur:</h2>';
+?>
+		<div class="container">
+			<div id="transparency">
+				<h3 id="post-title" class="mb-4">Suppression du compte utilisateur:</h3>
+<?php
 	}
 ?>
 
-<p>Souhaitez-vous vraiment procéder à la suppression de votre compte ?</p>
+				<p>Souhaitez-vous vraiment procéder à la suppression de votre compte ?</p>
 
-<form action="index.php" method="POST">
-	<p><input type="radio" name="delete_account" value="confirm" id="confirm"/> <label for="confirm">Oui</label></p>
-    <p><input type="radio" name="delete_account" value="cancel" id="cancel"/> <label for="cancel">Non</label></p>
-    <input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>"/>
-    <input type="hidden" name="id" value="<?= $_SESSION['id'] ?>"/>
-	<input type="submit" value="Confirmer"/>
-</form>
+				<form action="index.php" method="POST">
+					<p><input type="radio" name="delete_account" value="confirm" id="confirm"/> <label for="confirm">Oui</label></p>
+				    <p><input type="radio" name="delete_account" value="cancel" id="cancel"/> <label for="cancel">Non</label></p>
+				    <input type="hidden" name="pseudo" value="<?= $_SESSION['pseudo'] ?>"/>
+				    <input type="hidden" name="id" value="<?= $_SESSION['id'] ?>"/>
+					<input class="btn btn-primary" type="submit" value="Confirmer"/>
+				</form>
+			</div>
+		</div>
 
 <?php $content = ob_get_clean(); ?>
 

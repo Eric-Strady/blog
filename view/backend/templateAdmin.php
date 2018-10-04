c9b77feadafe520671006cc5a49f0f1dedbbf4ff
Eric-Strady <strady60@gmail.com>
2018-10-04 23:42:29 +0200

    change height for TinyMCE

--- view/backend/templateAdmin.php
+++ view/backend/templateAdmin.php
@@ -15,7 +15,7 @@
 		<script type="text/javascript">
         	tinymce.init({
         		selector: '#add_content',
-        		height: 500,
+        		height: 400,
 				theme: 'modern',
 				plugins: 'preview searchreplace autolink link pagebreak nonbreaking advlist lists textcolor wordcount contextmenu colorpicker help',
 				block_formats: 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6',
@@ -25,7 +25,7 @@
 		<script type="text/javascript">
         	tinymce.init({
         		selector: '#up_content',
-        		height: 500,
+        		height: 400,
 				theme: 'modern',
 				plugins: 'preview searchreplace autolink link pagebreak nonbreaking advlist lists textcolor wordcount contextmenu colorpicker help',
 				block_formats: 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6',

6487db2ffc3abe2394fa377d97d9eb969bfcd4cd
Eric-Strady <strady60@gmail.com>
2018-10-05 00:23:17 +0200

    add style for addPostView.php

--- view/backend/addPostView.php
+++ view/backend/addPostView.php
@@ -8,15 +8,20 @@
 	if (isset($_SESSION['admin']) AND $_SESSION['admin']=='ok')
  	{
 ?>
-		<h2>Création d'un billet:</h2>
+		<div class="container">
+			<div id="transparency">
+				<h3 id="post-title" class="mb-4">Création d'un billet:</h3>
 
-		<form action="index.php" method="POST">
-			<p><label for="add_title">Titre du billet:</label></p>
-			<p><input type="text" name="add_title" id="add_title" maxlength="255" size="50" placeholder="Ex: Episode ..." required autofocus/></p>
-			<p><label for="add_content">Contenu du billet:</label></p>
-		    <p><textarea name="add_content" id="add_content" required>Vous pouvez effectuer diverses actions syntaxiques (mise en gras, alignement, etc) à l'aide de cet éditeur de texte.</textarea></p>
-		    <input type="submit" value="Ajouter ce billet"/>
-		</form>
+				<form action="index.php" method="POST">
+					<p><label for="add_title">Titre du billet:</label></p>
+					<p><input class="form-control" type="text" name="add_title" id="add_title" maxlength="255" size="50" placeholder="Ex: Episode ..." required autofocus/></p>
+					<p><label for="add_content">Contenu du billet:</label></p>
+				    <p><textarea class="form-control" name="add_content" id="add_content" required>Vous pouvez effectuer diverses actions syntaxiques (mise en gras, alignement, etc) à l'aide de cet éditeur de texte.</textarea></p>
+				    <input type="submit" class="btn btn-primary" value="Ajouter ce billet"/>
+				</form>
+			</div>
+		</div>
+
 <?php
 	}
 	else

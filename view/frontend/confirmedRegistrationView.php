6c404a073d249b0b6fb83a053435a98e4c1d93bd
Eric-Strady <strady60@gmail.com>
2018-10-05 00:44:19 +0200

    add style for confirmedRegistration.php

--- view/frontend/confirmedRegistrationView.php
+++ view/frontend/confirmedRegistrationView.php
@@ -4,10 +4,14 @@
 
 <?php ob_start(); ?>
 
-<h1>Inscription finalisée</h1>
+<div class="container">
+	<div class="text-center mt-5">
+		<h1 class="h3 mb-3 font-weight-normal">Inscription finalisée</h1>
 
-<p>Félicitation !<br/>
-Votre inscription est désormais finalisée. Vous pouvez dès à présent vous <a href="index.php?link=connexion">connecter</a> ;)</p>
+		<p>Félicitation !<br/>
+		Votre inscription est désormais finalisée. Vous pouvez dès à présent vous <a href="index.php?link=connexion">connecter</a> ;)</p>
+	</div>
+</div>
 
 <?php $content = ob_get_clean(); ?>
 

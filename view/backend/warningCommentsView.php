eadf8b0907a71fb994430088df4886a4973149b0
Eric-Strady <strady60@gmail.com>
2018-10-05 17:47:43 +0200

    add style for warningCommentsView.php with transparency bg

--- view/backend/warningCommentsView.php
+++ view/backend/warningCommentsView.php
@@ -8,17 +8,21 @@
 	if (isset($_SESSION['admin']) AND $_SESSION['admin']=='ok')
  	{
 ?>
-		<h2>Commentaire(s) signalé(s):</h2>
+		<div class="container">
+			<div id="transparency">
+				<h3 id="post-title" class="mb-4">Commentaire(s) signalé(s):</h3>
 
-		<?php
-			while ($comment = $listWarnedComments->fetch())
-			{
-		?>
-				<p><em>Le <?= $comment['d_warning'] ?> à <?= $comment['h_warning'] ?></em> - <strong><?= strip_tags($comment['author']) ?></strong>: ( <a href="index.php?eraseComment=<?= $comment['id_comment'] ?>&amp;eraseWarning=<?= $comment['id'] ?>">Supprimer</a> - <a href="index.php?conserve=<?= $comment['id'] ?>">Conserver</a> )<br/>
-				<?= strip_tags($comment['comment'])?></p>
-		<?php
-			}
-		?>
+				<?php
+					while ($comment = $listWarnedComments->fetch())
+					{
+				?>
+						<p><em>Le <?= $comment['d_warning'] ?> à <?= $comment['h_warning'] ?></em> - <strong><?= strip_tags($comment['author']) ?></strong>: ( <a href="index.php?eraseComment=<?= $comment['id_comment'] ?>&amp;eraseWarning=<?= $comment['id'] ?>">Supprimer</a> - <a href="index.php?conserve=<?= $comment['id'] ?>">Conserver</a> )<br/>
+						<?= strip_tags($comment['comment'])?></p>
+				<?php
+					}
+				?>
+			</div>
+		</div>
 <?php
 	}
 	else

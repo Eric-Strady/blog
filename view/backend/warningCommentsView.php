eb5def1a479cd68ef7860f0c383300a5dc8fd3b5
Eric-Strady <strady60@gmail.com>
2018-10-11 17:08:48 +0200

    change style with class='card' for the page

--- view/backend/warningCommentsView.php
+++ view/backend/warningCommentsView.php
@@ -16,7 +16,9 @@
 					if ($listWarnedComments === 0)
 					{
 				?>
-						<p>Aucun commentaire signalé ! :)</p>
+						<div class="alert alert-secondary">
+							Aucun commentaire signalé ! :)
+						</div>
 				<?php
 					}
 					else
@@ -24,12 +26,16 @@
 						while ($comment = $listWarnedComments->fetch())
 						{
 				?>
-						<p>Signalé <?= $comment['nTimes'] ?> fois:</p>
-						<p><em>Le <?= $comment['d_warning'] ?> à <?= $comment['h_warning'] ?></em> - <strong><?= strip_tags($comment['author']) ?></strong>:
-						<a href="index.php?eraseComment=<?= $comment['id_comment'] ?>&amp;eraseWarning=<?= $comment['id'] ?>" title="Supprimer le commentaire"><span class="fas fa-times fa-lg"></a>
-						 - 
-						<a href="index.php?conserve=<?= $comment['id_comment'] ?>" title="Conserver le commentaire"><span class="fas fa-check fa-lg"></a><br/>
-						<?= strip_tags($comment['comment'])?></p>
+							<div class="card my-4">
+								<h5 class="card-header">Signalé <?= $comment['nTimes'] ?> fois:</h5>
+								<div class="card-body">
+									<p><em>Le <?= $comment['d_warning'] ?> à <?= $comment['h_warning'] ?></em> - Commentaire de "<strong><?= strip_tags($comment['author']) ?></strong>" :
+									<a href="index.php?eraseComment=<?= $comment['id_comment'] ?>&amp;eraseWarning=<?= $comment['id'] ?>" title="Supprimer le commentaire"><span class="fas fa-times fa-lg"></a>
+									 - 
+									<a href="index.php?conserve=<?= $comment['id_comment'] ?>" title="Conserver le commentaire"><span class="fas fa-check fa-lg"></a><br/>
+									<?= strip_tags($comment['comment'])?></p>
+								</div>
+							</div>
 				<?php
 						}
 					}

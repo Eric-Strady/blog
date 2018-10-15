de4f5f68ebe62df6efb7a1d429331517ca4b17d5
Eric-Strady <strady60@gmail.com>
2018-10-15 22:51:51 +0200

    change link's redirection to match with new architecture

--- controller/homeController.php
+++ controller/homeController.php
@@ -1,9 +1,45 @@
 <?php
 
+	require_once('model/Post.php');
 	require_once('model/PostsManager.php');
 
+	use \Eric\Blog\Model\Posts\Post;
 	use \Eric\Blog\Model\Posts\PostsManager;
 
+	$posts = new Post([]);
+	$postsManager = new PostsManager();
+
+	$max_nb_post = 6;
+	$total_post = $postsManager->count();
+	$nb_post = $total_post;
+	$nb_page = ceil($nb_post/$max_nb_post);
+
+	if (isset($_GET['page']))
+	{
+		strip_tags($_GET['page']);
+		$_GET['page'] = (int)$_GET['page'];
+		$current_page = $_GET['page'];
+		if ($current_page == 0)
+		{
+			$current_page = 1;
+		}
+		elseif ($current_page > $nb_page)
+		{
+			$current_page = $nb_page;
+		}
+	}
+	else
+	{
+		$current_page = 1;
+	}
+
+	$first_post = ($current_page-1)*$max_nb_post;
+
+	$posts = $postsManager->listPosts($first_post, $max_nb_post);
+
+	require 'view/frontend/listPostsView.php';
+
+	/*
 	//Affichage des posts + pagination pour listPostsView
 	function listPosts()
 	{
@@ -42,3 +78,4 @@
 
 		require('view/frontend/listPostsView.php');
 	}
+	*/

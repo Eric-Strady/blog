d143e76b6fd58695653d40b436353217d7abf814
Eric-Strady <strady60@gmail.com>
2018-10-17 19:44:52 +0200

    change parameter for moderate link + turn php code after this link on comment

--- view/backend/templateAdmin.php
+++ view/backend/templateAdmin.php
@@ -52,8 +52,8 @@
 			            	<a class="nav-link" href="index.php?link=crud&amp;action=create" title="Page de création">Ajouter un billet</a>
 			            </li>
 			            <li class="nav-item">
-			            	<a class="nav-link" href="index.php?link=crud&amp;action=moderate" title="Page de modération">
-			            		Modérer les commentaires <?php if ($nbWarning['nb_warning']!='0'){ echo'<span class="badge">' . $nbWarning['nb_warning'] . '</span>'; } ?>
+			            	<a class="nav-link" href="index.php?link=moderate" title="Page de modération">
+			            		Modérer les commentaires <!-- <?php //if ($nbWarning['nb_warning']!='0'){ echo'<span class="badge">' . $nbWarning['nb_warning'] . '</span>'; } ?> -->
 			            	</a>
 			            </li>
 			            <li class="nav-item">

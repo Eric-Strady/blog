e34074a24d68fcc6c2bacb4f492805a314939872
Eric-Strady <strady60@gmail.com>
2018-10-17 20:03:19 +0200

    transfer session verifications to controller

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -2,57 +2,45 @@
 
 <?php ob_start(); ?>
 
-	<?php
-		if (isset($_SESSION['admin']) AND $_SESSION['admin']=='ok')
-	 	{
-	?>
-			<div class="container">
-				<div id="transparency">
-					<div class="row">
-						<?php
-							foreach ($posts as $onePost)
-							{
-						?>
-							    <div class="col-lg-4 col-sm-6 portfolio-item">
-							      	<div class="card mb-4">
-							        	<a href="index.php?read=<?= $onePost->getId() ?>"><img class="card-img-top" src="public/images/cover/<?= $onePost->getId() ?>.<?= $onePost->getImgExt() ?>" alt="<?= $onePost->getImgDesc() ?>"/></a>
-							        	<div class="card-body">
-							      			<h4 class="card-title" id="post-title">
-							        			<a href="index.php?read=<?= $onePost->getId() ?>" title="Page du billet"><?= $onePost->getTitle() ?></a>
-							      			</h4>
-							      			<em> le <?= $onePost->getCreationDate() ?></em>
-							      			<em><a href="index.php?read=<?= $onePost->getId() ?>" title="Lecture du billet">Lire</a></em>
-							     			- <em><a href="index.php?update=<?= $onePost->getId() ?>" title="Modification du billet">Mettre à jour</a></em>
-							     			- <em><a href="index.php?delete=<?= $onePost->getId() ?>&amp;pic=<?= $onePost->getImgExt() ?>" title="Suppression du billet">Supprimer</a></em></p>
-							      			<p class="card-text"><?= substr($onePost->getContent(), 0, 45) . '...' ?></p>
-							        	</div>
-							      	</div>
-							    </div>
-						<?php
-							}
-						?>
-					</div>
-
-					<nav id="pagination" class="mt-4">
-						<ul class="pagination pagination-lg">
-							<?php
-								for ($i = 1; $i <= $nb_page; $i++)
-								{
-									echo '<li class="page-item"><a class="page-link" href="index.php?link=admin&amp;page=' . $i . '" title="Liens vers la page ' . $i . '">' . $i . '</a></li>';
-								}
-							?>
-						</ul>
-					</nav>
-				</div>
+	<div class="container">
+		<div id="transparency">
+			<div class="row">
+				<?php
+					foreach ($posts as $onePost)
+					{
+				?>
+					    <div class="col-lg-4 col-sm-6 portfolio-item">
+					      	<div class="card mb-4">
+					        	<a href="index.php?read=<?= $onePost->getId() ?>"><img class="card-img-top" src="public/images/cover/<?= $onePost->getId() ?>.<?= $onePost->getImgExt() ?>" alt="<?= $onePost->getImgDesc() ?>"/></a>
+					        	<div class="card-body">
+					      			<h4 class="card-title" id="post-title">
+					        			<a href="index.php?read=<?= $onePost->getId() ?>" title="Page du billet"><?= $onePost->getTitle() ?></a>
+					      			</h4>
+					      			<em> le <?= $onePost->getCreationDate() ?></em>
+					      			<em><a href="index.php?read=<?= $onePost->getId() ?>" title="Lecture du billet">Lire</a></em>
+					     			- <em><a href="index.php?update=<?= $onePost->getId() ?>" title="Modification du billet">Mettre à jour</a></em>
+					     			- <em><a href="index.php?delete=<?= $onePost->getId() ?>&amp;pic=<?= $onePost->getImgExt() ?>" title="Suppression du billet">Supprimer</a></em></p>
+					      			<p class="card-text"><?= substr($onePost->getContent(), 0, 45) . '...' ?></p>
+					        	</div>
+					      	</div>
+					    </div>
+				<?php
+					}
+				?>
 			</div>
 
-	<?php
-		}
-		else
-		{
-			throw new Exception('Vous n\'êtes pas autorisé à aller sur cette page !<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
-		}
-	?>
+			<nav id="pagination" class="mt-4">
+				<ul class="pagination pagination-lg">
+					<?php
+						for ($i = 1; $i <= $nb_page; $i++)
+						{
+							echo '<li class="page-item"><a class="page-link" href="index.php?link=admin&amp;page=' . $i . '" title="Liens vers la page ' . $i . '">' . $i . '</a></li>';
+						}
+					?>
+				</ul>
+			</nav>
+		</div>
+	</div>
 
 <?php $content = ob_get_clean(); ?>
 
--- view/backend/warningCommentsView.php
+++ view/backend/warningCommentsView.php
@@ -2,51 +2,40 @@
 
 <?php ob_start(); ?>
 
-<?php
-	if (isset($_SESSION['admin']) AND $_SESSION['admin']=='ok')
- 	{
-?>
-		<div class="container">
-			<div id="transparency">
-				<h3 id="post-title" class="mb-4">Commentaire(s) signalé(s):</h3>
+	<div class="container">
+		<div id="transparency">
+			<h3 id="post-title" class="mb-4">Commentaire(s) signalé(s):</h3>
 
-				<?php
-					if ($listWarnedComments === 0)
+			<?php
+				if ($listWarnedComments === 0)
+				{
+			?>
+					<div class="alert alert-secondary">
+						Aucun commentaire signalé ! :)
+					</div>
+			<?php
+				}
+				else
+				{
+					while ($comment = $listWarnedComments->fetch())
 					{
-				?>
-						<div class="alert alert-secondary">
-							Aucun commentaire signalé ! :)
-						</div>
-				<?php
-					}
-					else
-					{
-						while ($comment = $listWarnedComments->fetch())
-						{
-				?>
-							<div class="card my-4">
-								<h5 class="card-header">Signalé <?= $comment['nTimes'] ?> fois:</h5>
-								<div class="card-body">
-									<p><em>Le <?= $comment['d_warning'] ?> à <?= $comment['h_warning'] ?></em> - Commentaire de "<strong><?= strip_tags($comment['author']) ?></strong>" :
-									<a href="index.php?eraseComment=<?= $comment['id_comment'] ?>&amp;eraseWarning=<?= $comment['id'] ?>" title="Supprimer le commentaire"><span class="fas fa-times fa-lg"></a>
-									 - 
-									<a href="index.php?conserve=<?= $comment['id_comment'] ?>" title="Conserver le commentaire"><span class="fas fa-check fa-lg"></a><br/>
-									<?= strip_tags($comment['comment'])?></p>
-								</div>
+			?>
+						<div class="card my-4">
+							<h5 class="card-header">Signalé <?= $comment['nTimes'] ?> fois:</h5>
+							<div class="card-body">
+								<p><em>Le <?= $comment['d_warning'] ?> à <?= $comment['h_warning'] ?></em> - Commentaire de "<strong><?= strip_tags($comment['author']) ?></strong>" :
+								<a href="index.php?eraseComment=<?= $comment['id_comment'] ?>&amp;eraseWarning=<?= $comment['id'] ?>" title="Supprimer le commentaire"><span class="fas fa-times fa-lg"></a>
+								 - 
+								<a href="index.php?conserve=<?= $comment['id_comment'] ?>" title="Conserver le commentaire"><span class="fas fa-check fa-lg"></a><br/>
+								<?= strip_tags($comment['comment'])?></p>
 							</div>
-				<?php
-						}
+						</div>
+			<?php
 					}
-				?>
-			</div>
+				}
+			?>
 		</div>
-<?php
-	}
-	else
-	{
-		throw new Exception('Vous n\'êtes pas autorisé à aller sur cette page !<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
-	}
-?>
+	</div>
 
 <?php $content = ob_get_clean(); ?>
 

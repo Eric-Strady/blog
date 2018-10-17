88abbfa6d3d75ba1da4c9ea7e6b31af9584ed4db
Eric-Strady <strady60@gmail.com>
2018-10-17 19:08:20 +0200

    change variables by objects

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -10,21 +10,21 @@
 				<div id="transparency">
 					<div class="row">
 						<?php
-							while ($data = $req->fetch())
+							foreach ($posts as $onePost)
 							{
 						?>
 							    <div class="col-lg-4 col-sm-6 portfolio-item">
 							      	<div class="card mb-4">
-							        	<a href="index.php?read=<?= $data['id'] ?>"><img class="card-img-top" src="public/images/cover/<?= $data['id'] ?>.<?= $data['image_extension'] ?>" alt="<?= $data['image_description'] ?>"/></a>
+							        	<a href="index.php?read=<?= $onePost->getId() ?>"><img class="card-img-top" src="public/images/cover/<?= $onePost->getId() ?>.<?= $onePost->getImgExt() ?>" alt="<?= $onePost->getImgDesc() ?>"/></a>
 							        	<div class="card-body">
 							      			<h4 class="card-title" id="post-title">
-							        			<a href="index.php?read=<?= $data['id'] ?>" title="Page du billet"><?= strip_tags($data['title']) ?></a>
+							        			<a href="index.php?read=<?= $onePost->getId() ?>" title="Page du billet"><?= $onePost->getTitle() ?></a>
 							      			</h4>
-							      			<em> le <?= $data['creation_date_fr'] ?></em>
-							      			<em><a href="index.php?read=<?= $data['id'] ?>" title="Lecture du billet">Lire</a></em>
-							     			- <em><a href="index.php?update=<?= $data['id'] ?>" title="Modification du billet">Mettre à jour</a></em>
-							     			- <em><a href="index.php?delete=<?= $data['id'] ?>&amp;pic=<?= $data['image_extension'] ?>" title="Suppression du billet">Supprimer</a></em></p>
-							      			<p class="card-text"><?= substr($data['content'], 0, 45) . '...' ?></p>
+							      			<em> le <?= $onePost->getCreationDate() ?></em>
+							      			<em><a href="index.php?read=<?= $onePost->getId() ?>" title="Lecture du billet">Lire</a></em>
+							     			- <em><a href="index.php?update=<?= $onePost->getId() ?>" title="Modification du billet">Mettre à jour</a></em>
+							     			- <em><a href="index.php?delete=<?= $onePost->getId() ?>&amp;pic=<?= $onePost->getImgExt() ?>" title="Suppression du billet">Supprimer</a></em></p>
+							      			<p class="card-text"><?= substr($onePost->getContent(), 0, 45) . '...' ?></p>
 							        	</div>
 							      	</div>
 							    </div>
@@ -33,15 +33,12 @@
 						?>
 					</div>
 
-					<?php
-						$req->closeCursor();
-					?>
 					<nav id="pagination" class="mt-4">
 						<ul class="pagination pagination-lg">
 							<?php
 								for ($i = 1; $i <= $nb_page; $i++)
 								{
-									echo '<li class="page-item"><a class="page-link" href="index.php?show=' . $i . '" title="Liens vers la page ' . $i . '">' . $i . '</a></li>';
+									echo '<li class="page-item"><a class="page-link" href="index.php?link=admin&amp;page=' . $i . '" title="Liens vers la page ' . $i . '">' . $i . '</a></li>';
 								}
 							?>
 						</ul>

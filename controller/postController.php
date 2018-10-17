38112640348678d3bc9de5dadac971cfdaed389e
Eric-Strady <strady60@gmail.com>
2018-10-17 18:20:09 +0200

    add new action + add repportComment()

--- controller/postController.php
+++ controller/postController.php
@@ -4,11 +4,17 @@
 	require_once('model/PostsManager.php');
 	require_once('model/Comment.php');
 	require_once('model/CommentsManager.php');
+	require_once('model/UsersManager.php');
+	require_once('model/Warning.php');
+	require_once('model/WarningManager.php');
 
 	use \Eric\Blog\Model\Posts\Post;
 	use \Eric\Blog\Model\Posts\PostsManager;
 	use \Eric\Blog\Model\Comments\Comment;
 	use \Eric\Blog\Model\Comments\CommentsManager;
+	use \Eric\Blog\Model\Users\UsersManager;
+	use \Eric\Blog\Model\Warning\Warning;
+	use \Eric\Blog\Model\Warning\WarningManager;
 
 												//DEFINE ACTION
 
@@ -27,6 +33,10 @@
 
 			case 'update_comment':
 				changeComment();
+			break;
+
+			case 'warning':
+				reportComment();
 			break;
 
 			default:
@@ -100,13 +110,12 @@
 
 	function changeComment()
 	{
-		if (isset($_POST['up_comment'], $_POST['id_comment'], $_POST['id_post']))
+		if (isset($_POST['up_comment'], $_POST['id_comment']))
 		{
-			if ($_POST['up_comment']!='' AND $_POST['id_comment']!='' AND $_POST['id_post']!='')
+			if ($_POST['up_comment']!='' AND $_POST['id_comment']!='')
 			{
 				$up_comment = strip_tags($_POST['up_comment']);
 				$id = $_POST['id_comment'];
-				$id_post = $_POST['id_post'];
 
 				$comment = new Comment(['comment' => $up_comment, 'id' => $id]);
 				$commentsManager = new CommentsManager();
@@ -120,7 +129,7 @@
 				}
 				else
 				{
-					throw new Exception('<p>Impossible de modifier ce commentaire !<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
+					throw new Exception('<p>Ce commentaire n\'existe pas !<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
 				}
 			}
 			else
@@ -131,6 +140,54 @@
 		else
 		{
 			throw new Exception('<p>Vous devez renseignez tous les champs.<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
+		}
+	}
+
+	function reportComment()
+	{
+		if (isset($_GET['id_comment'], $_GET['id_user'], $_GET['id_post']))
+		{
+			if ($_GET['id_comment']!='' AND $_GET['id_user']!='' AND $_GET['id_post']!='')
+			{
+				$id_comment = $_GET['id_comment'];
+				$id_user = $_GET['id_user'];
+				$id_post = $_GET['id_post'];
+
+				$warning = new Warning(['id_comment' => $id_comment, 'id_user' => $id_user, 'id_post' => $id_post]);
+				$usersManager = new UsersManager();
+				$commentsManager = new CommentsManager();
+				$postsManager = new PostsManager();
+
+				if ($usersManager->isExist($warning->getIdUser()) AND $commentsManager->isExist($warning->getIdComment()) AND $postsManager->isExist($warning->getIdPost()))
+				{
+					$warningManager = new WarningManager();
+
+					if (!$warningManager->alreadyWarned($warning->getIdUser(), $warning->getIdComment()))
+					{
+						$warningManager->addWarning($warning->getIdUser(), $warning->getIdComment(), $warning->getIdPost());
+
+						setcookie('warned', 'done', time()+30, null, null, false, true);
+						$path = 'Location: http://127.0.0.1/blog/index.php?link=post&action=read&id=' . $warning->getIdPost() . '#comments';
+						header($path);
+					}
+					else
+					{
+						throw new Exception('<p>Vous avez déjà signalé ce commentaire. Il sera traité dans les plus brefs délais.<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
+					}
+				}
+				else
+				{
+					throw new Exception('<p>L\'utilisateur ou le commentaire n\'existe pas !<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
+				}
+			}
+			else
+			{
+				throw new Exception('<p>Impossible de signaler ce commentaire.<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
+			}
+		}
+		else
+		{
+			throw new Exception('<p>Vous devez renseignez les identifiants du commentaire et de l\'utilisateur.<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
 		}
 	}
 

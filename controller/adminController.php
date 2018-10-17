e54b7b0960cc02ceeca13cca96d25399513e440a
Eric-Strady <strady60@gmail.com>
2018-10-17 19:13:14 +0200

    add system to display posts and pagination

--- controller/adminController.php
+++ controller/adminController.php
@@ -1,11 +1,47 @@
 <?php
 
+	require_once('model/Post.php');
 	require_once('model/PostsManager.php');
-	require_once('model/WarningManager.php');
 
+	use \Eric\Blog\Model\Posts\Post;
 	use \Eric\Blog\Model\Posts\PostsManager;
-	use \Eric\Blog\Model\Warning\WarningManager;
 
+
+	$posts = new Post([]);
+	$postsManager = new PostsManager();
+
+	$max_nb_post = 6;
+	$total_post = $postsManager->count();
+	$nb_post = $total_post;
+	$nb_page = ceil($nb_post/$max_nb_post);
+
+	if (isset($_GET['page']))
+	{
+		$_GET['page'] = (int) $_GET['page'];
+		$current_page = $_GET['page'];
+		if ($current_page == 0)
+		{
+			$current_page = 1;
+		}
+		elseif ($current_page > $nb_page)
+		{
+			$current_page = $nb_page;
+		}
+	}
+	else
+	{
+		$current_page = 1;
+	}
+
+	$first_post = ($current_page-1)*$max_nb_post;
+
+	$posts = $postsManager->listPosts($first_post, $max_nb_post);
+
+	require 'view/backend/adminView.php';
+	
+
+		
+	/*
 		//Affichage liste des posts + pagination pour adminView.php
 	function listPostsAdmin()
 	{
@@ -49,3 +85,4 @@
 
 		require('view/backend/adminView.php');
 	}
+	*/

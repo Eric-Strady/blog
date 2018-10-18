61477cdc1662f1c493053cf7efb1422d49cb0e07
Eric-Strady <strady60@gmail.com>
2018-10-18 13:31:28 +0200

    delete session_start + session verification + modify array results by objects

--- view/backend/readPostView.php
+++ view/backend/readPostView.php
@@ -1,42 +1,28 @@
-<?php session_start(); ?>
-
 <?php $title = 'Blog de Jean Forteroche'; ?>
 
 <?php ob_start(); ?>
 
-<?php
-	if (isset($_SESSION['admin']) AND $_SESSION['admin']=='ok')
- 	{
-?>
-		<div class="container">
-			<div id="transparency-post">
-				<div class="row">
-				    <div class="col-lg-12">
+	<div class="container">
+		<div id="transparency-post">
+			<div class="row">
+			    <div class="col-lg-12">
 
-						<h1 id="post-title"><?= strip_tags($post['title']) ?></h1>
+					<h1 id="post-title"><?= $post->getTitle() ?></h1>
 
-						<p>Posté le <?= $post['creation_date_fr'] ?></p>
+					<p>Posté le <?= $post->getCreationDate() ?></p>
 
-						<hr>
+					<hr>
 
-						<img id="imagePost" class="img-fluid rounded" src="public/images/cover/<?= $post['id'] ?>.<?= $post['image_extension'] ?>" alt="<?= $post['image_description'] ?>" width="600" height="200"/>
+					<img id="imagePost" class="img-fluid rounded" src="public/images/cover/<?= $post->getId() ?>.<?= $post->getImgExt() ?>" alt="<?= $post->getImgDesc() ?>" width="600" height="200"/>
 
-						<hr>
+					<hr>
 
-						<p><?= nl2br($post['content']) ?></p>
+					<p><?= nl2br($post->getContent()) ?></p>
 
-					</div>
 				</div>
 			</div>
 		</div>
-
-<?php
-	}
-	else
-	{
-		throw new Exception('Vous n\'êtes pas autorisé à aller sur cette page !<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
-	}
-?>
+	</div>
 
 <?php $content = ob_get_clean(); ?>
 

0b8e584cdb832f5396206d9e1203a5540ab86791
Eric-Strady <strady60@gmail.com>
2018-10-18 13:56:05 +0200

    add chargePost() and changeImg()

--- controller/crudController.php
+++ controller/crudController.php
@@ -28,10 +28,14 @@
 				break;
 
 				case 'update_page':
-					require 'view/backend/updatePostView.php';
+					chargePost();
 				break;
 
-				case 'update':
+				case 'update_img':
+					changeImg();
+				break;
+
+				case 'update_post':
 					changePost();
 				break;
 
@@ -137,6 +141,93 @@
 		else
 		{
 			throw new Exception('<p>Vous devez renseigné l\'identifiant du billet.<br/>Retour à l\'<a href="index.php?link=admin" title="Interface d\'administration" class="alert-link">interface d\'administration</a></p>');
+		}
+	}
+
+	function chargePost()
+	{
+		if (isset($_GET['id_post']) AND $_GET['id_post']!='')
+		{
+			$id_post = $_GET['id_post'];
+			$post = new Post(['id' => $id_post]);
+			$postsManager = new PostsManager();
+
+			if ($postsManager->isExist($post->getId()))
+			{
+				$post = $postsManager->findPost($post->getId());
+
+				require 'view/backend/updatePostView.php';
+			}
+			else
+			{
+				throw new Exception('<p>Ce billet n\'existe pas !<br/>Retour à l\'<a href="index.php?link=admin" title="Interface d\'administration" class="alert-link">interface d\'administration</a></p>');
+			}
+		}
+		else
+		{
+			throw new Exception('<p>Vous devez renseigné l\'identifiant du billet.<br/>Retour à l\'<a href="index.php?link=admin" title="Interface d\'administration" class="alert-link">interface d\'administration</a></p>');
+		}
+	}
+
+	function changeImg()
+	{
+		if (isset($_POST['description'], $_FILES['image'], $_POST['id_post']))
+		{
+			if ($_POST['description']!='' AND $_FILES['image']!='' AND $_POST['id_post']!='')
+			{
+				if ($_FILES['image']['error'] == 0)
+				{
+					if ($_FILES['image']['size']<=500000)
+					{
+						$data_files = pathinfo($_FILES['image']['name']);
+						$extension_upload = $data_files['extension'];
+						$authorized_extensions = array('jpg', 'jpeg', 'png');
+
+						if (in_array($extension_upload, $authorized_extensions))
+						{
+							$description = strip_tags($_POST['description']);
+							$id_post = $_POST['id_post'];
+
+							$post = new Post(['id' => $id_post, 'image_description' => $description, 'image_extension' => $extension_upload]);
+							$postsManager = new PostsManager();
+
+							if ($postsManager->isExist($post->getId()))
+							{
+								$postsManager->updateImage($post);
+
+								move_uploaded_file($_FILES['image']['tmp_name'], 'public/images/cover/' . $post->getId() . '.' . $post->getImgExt());
+
+								$path = 'Location: http://127.0.0.1/blog/index.php?link=crud&action=read&id_post=' . $post->getId();
+								header($path);
+							}
+							else
+							{
+								throw new Exception('<p>Ce billet n\'existe pas !<br/>Retour à l\'<a href="index.php?link=admin" title="Interface d\'administration" class="alert-link">interface d\'administration</a></p>');
+							}
+						}
+						else
+						{
+							throw new Exception('<p>Le format de l\'image n\'est pas conforme. Pour rappel, vous devez transmettre une image au format "JPG", "JPEG" ou "PNG".<br/>Retour à l\'<a href="index.php?link=admin" title="Interface d\'administration" class="alert-link">interface d\'administration</a></p>');
+						}
+					}
+					else
+					{
+						throw new Exception('<p>L\'image est trop volumineuse. Pour rappel, elle ne doit pas dépasser 500Ko.<br/>Retour à l\'<a href="index.php?link=admin" title="Interface d\'administration" class="alert-link">interface d\'administration</a></p>');
+					}
+				}
+				else
+				{
+					throw new Exception('<p>Une erreur est survenue lors du téléchargement.<br/>Retour à l\'<a href="index.php?link=admin" title="Interface d\'administration" class="alert-link">interface d\'administration</a></p>');
+				}
+			}
+			else
+			{
+				throw new Exception('<p>Vous devez renseignez tous les champs.<br/>Retour à l\'<a href="index.php?link=admin" title="Interface d\'administration" class="alert-link">interface d\'administration</a></p>');
+			}
+		}
+		else
+		{
+			throw new Exception('<p>Vous devez renseignez tous les champs.<br/>Retour à l\'<a href="index.php?link=admin" title="Interface d\'administration" class="alert-link">interface d\'administration</a></p>');
 		}
 	}
 

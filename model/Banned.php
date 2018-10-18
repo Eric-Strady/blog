6a7c514e4c414bf1bb74168fd13c4da57b55b954
Eric-Strady <strady60@gmail.com>
2018-10-18 18:03:58 +0200

    corrected mistakes in getters and add sendBannedEmail()

--- model/Banned.php
+++ model/Banned.php
@@ -25,12 +25,12 @@
 
         public function getEmail()
         {
-            return $this->_id;
+            return $this->_email;
         }
         
         public function getReasons()
         {
-            return $this->_pseudo;
+            return $this->_reasons;
         }
         
         public function setEmail($email)
@@ -61,5 +61,32 @@
                     throw new \Exception('<p>La taille du motif de suppression ne doit pas dépasser 255 caractères.<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
                 }
             }
+        }
+
+        public function sendBannedEmail()
+        {
+            $to = $this->getEmail();
+            $subject = 'Suppression de votre compte utilisateur';
+            $message = '
+                <html>
+                    <head></head>
+                    <body>
+                        <div align="center">
+                            <h3>Suppression de votre compte !</h3>
+                            <p>Bonjour,<br/>
+                            Votre compte utilisateur sur le blog de Jean Forteroche a fait l\'objet d\'une suppression par l\'administrateur.</p>
+                            <p>En voici les raisons:<br/>
+                            "' . $this->getReasons() . '"</p>
+                        </div>
+                    </body>
+                </html>
+            ';
+            $header = "From: \"Jean Forteroche\"<test.coxus@gmail.com>\n";
+            $header.= "Reply-to: \"Jean Forteroche\" <test.coxus@gmail.com>\n";
+            $header.= "MIME-Version: 1.0\n";
+            $header.= "Content-Type: text/html; charset=\"UTF-8\"";
+            $header.= "Content-Transfer-Encoding: 8bit";
+
+            mail($to, $subject, $message, $header);
         }
     }

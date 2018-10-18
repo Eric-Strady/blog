2a28d756dd9de666cbeab0cd66649e1c33553961
Eric-Strady <strady60@gmail.com>
2018-10-18 13:34:26 +0200

    delete session_start and session verifications

--- view/backend/deletePostView.php
+++ view/backend/deletePostView.php
@@ -1,35 +1,22 @@
-<?php session_start(); ?>
-
 <?php $title = 'Blog de Jean Forteroche'; ?>
 
 <?php ob_start(); ?>
 
-<?php
-	if (isset($_SESSION['admin']) AND $_SESSION['admin']=='ok')
- 	{
-?>
-		<div class="container">
-			<div id="transparency">
-				<h3 id="post-title" class="mb-4">Suppression du billet:</h3>
+	<div class="container">
+		<div id="transparency">
+			<h3 id="post-title" class="mb-4">Suppression du billet:</h3>
 
-				<p>Souhaitez-vous vraiment procéder à la suppression de ce billet ?</p>
+			<p>Souhaitez-vous vraiment procéder à la suppression de ce billet ?</p>
 
-				<form action="index.php" method="POST">
-					<p><input type="radio" name="delete" value="confirm" id="confirm" /> <label for="confirm">Oui</label></p>
-				    <p><input type="radio" name="delete" value="cancel" id="cancel" /> <label for="cancel">Non</label></p>
-				    <input type="hidden" name="id" value="<?= $_GET['delete'] ?>">
-				    <input type="hidden" name="extension" value="<?= $_GET['pic'] ?>">
-					<input class="btn btn-primary" type="submit" value="Confirmer"/>
-				</form>
-			</div>
+			<form action="index.php" method="POST">
+				<p><input type="radio" name="delete" value="confirm" id="confirm" /> <label for="confirm">Oui</label></p>
+			    <p><input type="radio" name="delete" value="cancel" id="cancel" /> <label for="cancel">Non</label></p>
+			    <input type="hidden" name="id" value="<?= $_GET['delete'] ?>">
+			    <input type="hidden" name="extension" value="<?= $_GET['pic'] ?>">
+				<input class="btn btn-primary" type="submit" value="Confirmer"/>
+			</form>
 		</div>
-<?php
-	}
-	else
-	{
-		throw new Exception('Vous n\'êtes pas autorisé à aller sur cette page !<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
-	}
-?>
+	</div>
 
 <?php $content = ob_get_clean(); ?>
 
--- view/backend/updatePostView.php
+++ view/backend/updatePostView.php
@@ -1,46 +1,32 @@
-<?php session_start(); ?>
-
 <?php $title = 'Blog de Jean Forteroche'; ?>
 
 <?php ob_start(); ?>
 
-<?php
-	if (isset($_SESSION['admin']) AND $_SESSION['admin']=='ok')
- 	{
-?>
-		<div class="container">
-			<div id="transparency">
-				<h3 id="post-title" class="mb-4">Changer l'image d'illustration</h3>
+	<div class="container">
+		<div id="transparency">
+			<h3 id="post-title" class="mb-4">Changer l'image d'illustration</h3>
 
-				<form class="mb-5" action="index.php" method="POST" enctype="multipart/form-data">
-					<p><label for="description">Description:</label><br/>
-					<input type="text" class="form-control" name="description" id="description" placeholder="Maximum 30 caractères" maxlength="30" required/></p>
-					<p><label for="image">Choisissez votre image (max 500Ko):</label><br/>
-					<input type="file" name="image" id="image" required/></p>
-					<input type="hidden" name="id" value="<?= $post['id'] ?>">
-					<input type="submit" class="btn btn-primary" value="Modifier l'image"/>
-				</form>
-				
-				<h3 id="post-title" class="mb-5">Modification du billet:</h3>
+			<form class="mb-5" action="index.php" method="POST" enctype="multipart/form-data">
+				<p><label for="description">Description:</label><br/>
+				<input type="text" class="form-control" name="description" id="description" placeholder="Maximum 30 caractères" maxlength="30" required/></p>
+				<p><label for="image">Choisissez votre image (max 500Ko):</label><br/>
+				<input type="file" name="image" id="image" required/></p>
+				<input type="hidden" name="id" value="<?= $post['id'] ?>">
+				<input type="submit" class="btn btn-primary" value="Modifier l'image"/>
+			</form>
+			
+			<h3 id="post-title" class="mb-5">Modification du billet:</h3>
 
-				<form action="index.php" method="POST">
-					<p><label for="up_title">Titre du billet:</label></p>
-					<p><input type="text" class="form-control" name="up_title" id="up_title" maxlength="255" size="50" value="<?= $post['title'] ?>" required/></p>
-					<p><label for="up_content">Contenu du billet:</label></p>
-			        <p><textarea name="up_content" id="up_content" required><?= $post['content'] ?></textarea></p>
-			        <input type="hidden" name="id" value="<?= $post['id'] ?>">
-			        <input type="submit" class="btn btn-primary" value="Modifier le billet"/>
-				</form>
-			</div>
+			<form action="index.php" method="POST">
+				<p><label for="up_title">Titre du billet:</label></p>
+				<p><input type="text" class="form-control" name="up_title" id="up_title" maxlength="255" size="50" value="<?= $post['title'] ?>" required/></p>
+				<p><label for="up_content">Contenu du billet:</label></p>
+		        <p><textarea name="up_content" id="up_content" required><?= $post['content'] ?></textarea></p>
+		        <input type="hidden" name="id" value="<?= $post['id'] ?>">
+		        <input type="submit" class="btn btn-primary" value="Modifier le billet"/>
+			</form>
 		</div>
-		
-<?php
-	}
-	else
-	{
-		throw new Exception('Vous n\'êtes pas autorisé à aller sur cette page !<br/>Retour à la page d\'<a href="index.php" title="Page d\'accueil" class="alert-link">accueil</a></p>');
-	}
-?>
+	</div>
 
 <?php $content = ob_get_clean(); ?>
 

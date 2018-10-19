74756b12b7393ff7d7d78df65ed4eda104f64b3e
Eric-Strady <strady60@gmail.com>
2018-10-19 11:11:55 +0200

    delete commentary

--- view/backend/templateAdmin.php
+++ view/backend/templateAdmin.php
@@ -52,9 +52,7 @@
 			            	<a class="nav-link" href="index.php?link=crud&amp;action=create_page" title="Page de création">Ajouter un billet</a>
 			            </li>
 			            <li class="nav-item">
-			            	<a class="nav-link" href="index.php?link=moderate&amp;action=list" title="Page de modération">
-			            		Modérer les commentaires <!-- <?php //if ($nbWarning['nb_warning']!='0'){ echo'<span class="badge">' . $nbWarning['nb_warning'] . '</span>'; } ?> -->
-			            	</a>
+			            	<a class="nav-link" href="index.php?link=moderate&amp;action=list" title="Page de modération">Modérer les commentaires</a>
 			            </li>
 			            <li class="nav-item">
 			            	<a class="nav-link" href="index.php" title="Page d'accueil">Visualiser le site</a>
